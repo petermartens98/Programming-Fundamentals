c3b9f59daaf88a7137c99c62968986f11fcb422f
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-09 08:49:52 -0400

    Create ArithmeticOperators.cpp

---
+++ C++/BroCode/ArithmeticOperators.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() 
+{
+   int students = 20;
+
+   // addition
+   // ------------------
+   //students = students + 1;
+   //students+=1;
+   //students++;
+
+   // subtraction
+   // ------------------
+   //students = students - 1;
+   //students-=1;
+   //students--;
+
+   // multiplication
+   // ------------------
+   //students = students * 2;
+   //students*=2;
+
+   // division
+   // ------------------
+   //students = students / 2;
+   //students/=2;
+
+   // modulus
+   // ------------------
+   //int remainder = students % 3;
+   //std::cout << remainder;
+
+   std::cout << students;
+
+   return 0;
+}
+
