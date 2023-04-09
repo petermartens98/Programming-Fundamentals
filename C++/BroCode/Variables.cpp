383d8720b8650441604e72a6d9f99e4384c02db8
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-08 20:32:58 -0400

    Create Variables.cpp

---
+++ C++/BroCode/Variables.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+
+    //int (whole number)
+    int age = 24;
+    int year = 2023;
+    int days = 7;
+
+    //double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    //char (single character)
+    char grade = 'A';
+    char initial = 'C';
+    char currency = '$';
+
+    //boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    //string (objects that represent a sequence of text)
+    std::string name = "Peter";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+}
