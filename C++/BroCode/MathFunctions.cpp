24b9c3da33f33f4b76419743217fba8a03f6693c
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-09 08:57:40 -0400

    Create MathFunctions.cpp

---
+++ C++/BroCode/MathFunctions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main() 
+{
+   double x = 3.99;
+   double y = 4;
+   double z;
+
+   //z = std::max(x, y);
+   //z = std::min(x, y);
+   //z = pow(2, 4);
+   //z = sqrt(9);
+   //z = abs(-3);
+   //z = round(x);
+   //z = ceil(x);
+   //z = floor(x);
+
+   std::cout << z;
+
+   return 0;
+}
