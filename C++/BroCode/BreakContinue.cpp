08ba38404f8d13c0cea4c23d8915e24de4fcaf9b
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-10 15:58:55 -0400

    Create BreakContinue.cpp

---
+++ C++/BroCode/BreakContinue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    // break = break out of a loop
+    // continue = skip current iteration
+
+    for(int i = 1; i <= 20; i++){
+        if(i == 13){
+            //break;
+            //continue;
+        }
+        std::cout << i << '\n';
+    }
+}
