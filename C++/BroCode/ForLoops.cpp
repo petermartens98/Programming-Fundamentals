0f25c682bfbed029d30aacfc43d58e76e8529431
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-10 15:56:00 -0400

    Create ForLoops.cpp

---
+++ C++/BroCode/ForLoops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+int main()
+{
+   for(int i = 10; i > 0; i--){
+      //count down to 10
+      std::cout << i << '\n';
+   }
+ 
+   std::cout << "HAPPY NEW YEAR!\n";  
+ 
+   return 0;
+}
