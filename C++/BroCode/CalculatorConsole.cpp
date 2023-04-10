5959f55fa0b097716ddf5cc5d9c89ddf39eae6f4
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-10 14:32:02 -0400

    Create CalculatorConsole.cpp

---
+++ C++/BroCode/CalculatorConsole.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+
+int main() {
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "********* CALCULATOR ***********\n";
+
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '/':
+            result = num1 / num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        default:
+            std::cout << "That wasn't a valid operator\n";
+            break;
+    }
+
+    std::cout << "**********************************";
+
+    return 0;
+}
