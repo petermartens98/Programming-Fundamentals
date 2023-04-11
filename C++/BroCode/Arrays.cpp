30f84d285d98cc2075f2570486b29812937ed5d2
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-11 09:04:11 -0400

    Create Arrays.cpp

---
+++ C++/BroCode/Arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::string cars[3] = {"Corvette", "Mustang", "Camry"};
+
+    std::cout << cars[0] << '\n';
+    std::cout << cars[1] << '\n';
+    std::cout << cars[2] << '\n';
+
+    return 0;
+}
