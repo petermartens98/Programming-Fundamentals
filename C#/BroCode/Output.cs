34897ee5f0467968a5f4b0d398b65e0dc566a2e6
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-11 13:14:02 -0400

    Create Output.cs

---
+++ C#/BroCode/Output.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace OutputFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Yo");
+            Console.WriteLine("Hello World");
+
+            // This is a comment
+
+            /*
+             * Multiline Comment
+             */
+
+            Console.WriteLine("\tBroCode");
+            Console.WriteLine("Bro\bCode");
+            Console.WriteLine("Bro\nCode");
+
+            Console.ReadKey();
+        }
+    }
+}
