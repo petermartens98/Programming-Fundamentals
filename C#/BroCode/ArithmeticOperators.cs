7a498cc026036839b004ae5b2286c88efec7b42c
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-11 13:56:30 -0400

    Create ArithmeticOperators.cs

---
+++ C#/BroCode/ArithmeticOperators.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 5;
+
+            friends = friends + 1;
+            //friends += 1;
+            //friends++;
+
+            //friends = friends - 1;
+            //friends -= 1;
+            //friends--;
+
+            //friends = friends * 2;
+            //friends *= 2;
+
+            //friends = friends / 2;
+            //friends /= 2;
+
+            //int remainder = friends % 2;
+            //Console.WriteLine(remainder);
+
+            Console.WriteLine(friends);
+
+            Console.ReadKey();
+        }
+    }
+}
