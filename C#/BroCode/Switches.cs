b591efc9116433859fe70abcd4555b4b4f9152f8
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-11 14:00:13 -0400

    Create Switches.cs

---
+++ C#/BroCode/Switches.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Switches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // switch = an efficient alternative to many else if statements
+
+            Console.WriteLine("What day is it today?");
+            String day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("It's Wednesday!");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("It's Thursday!");
+                    break;
+                case "Friday":
+                    Console.WriteLine("It's Friday!");
+                    break;
+                case "Saturday":
+                    Console.WriteLine("It's Saturday!");
+                    break;
+                case "Sunday":
+                    Console.WriteLine("It's Sunday!");
+                    break;
+                default:
+                    Console.WriteLine(day + " is not a day!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
