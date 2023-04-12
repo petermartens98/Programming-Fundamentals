0072511f9b088c6a14e0c9cab3939e74c39a0059
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-12 19:39:27 -0400

    Create HelloWorldWithTime.js

---
+++ JavaScript/React/HelloWorldWithTime.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>The current time is {time.toLocaleTimeString()}.</p>
+      <footer>Created by Peter Martens</footer>
+    </div>
+  );
+};
+
+export default App;
