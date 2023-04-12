56276b44333b566e8423494884819d96593261d2
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-12 19:48:54 -0400

    Create HelloWorld.js

---
+++ JavaScript/React/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <footer>Created by Peter Martens</footer>
+    </div>
+  );
+};
+
+export default App;
