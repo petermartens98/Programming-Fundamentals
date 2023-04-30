6c082b33f6924eaa1004f9f6b548775bc0421c99
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-30 13:34:14 -0400

    Update useStateEx1.js

--- JavaScript/React/ReactHooks/useStateEx1.js
+++ JavaScript/React/ReactHooks/useStateEx1.js
@@ -1,1 +1,65 @@
+/*
+
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [state, setState] = useState({ count:4, theme:'blue'})
+  const count = state.count
+  const theme = state.theme
+
+  function decrementCount() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count - 1}
+    });
+  };
+  
+  function incrementCount() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count + 1}
+    });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+}
+
+export default App;
+
+*/
+
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+    setTheme("red");
+  };
+  
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+    setTheme("green");
+  };
+
+  return (
+    <div className="App">
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
+}
+
+export default App;
 
